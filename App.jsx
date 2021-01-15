a1cff310a450e17ab634fd2f79a6161744b29297
saaasi0716 <satoshi605@gmail.com>
2021-01-15 18:02:02 +0900

    New File

--- App.jsx
+++ App.jsx
@@ -1,12 +1,12 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import Hello from './src/components/Hello';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>Hello!</Text>
+      <Hello />
     </View>
   );
 }
@@ -19,3 +19,4 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
---
+++ src/components/Hello.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+function Hello() {
+  return (
+    <View>
+      <Text>Greatfuldays!</Text>
+    </View>
+  );
+}
+
+export default Hello;
