95851c8aec2a372da94230b325a5d63067951dc5
saaasi0716 <satoshi605@gmail.com>
2021-02-12 06:45:23 +0900

    Install React-native-webview

--- App.jsx
+++ App.jsx
@@ -1,14 +1,13 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import firebase from 'firebase';
+import { StyleSheet, Text, View, } from 'react-native';
+import {}from
 import Hello from './src/components/Hello';
-
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hello!</Text>
-      <Hello />
+      <Text>Hello!NewpositibMInd!</Text>
+      <Hello>World</Hello>
     </View>
   );
 }
@@ -21,18 +20,3 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
-
-
-
-const firebaseConfig = {
-  apiKey: 'AIzaSyCQ80I2QwSqZ1xrlW2z5WVmUPrTZO_n8Qg',
-  authDomain: 'kakeiboapp-270db.firebaseapp.com',
-  projectId: 'kakeiboapp-270db',
-  storageBucket: 'kakeiboapp-270db.appspot.com',
-  messagingSenderId: '872455420062',
-  appId: '1:872455420062:web:32a953d541139e3607638f',
-  measurementId: 'G-VJWC08Q94M'
-};
-if (firebase.app.lemgth === 0) {
- firebase.initializeApp(firebaseConfig)
-}
--- src/components/Hello.jsx
+++ src/components/Hello.jsx
@@ -1,13 +1,21 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { string } from 'prop-types'
 
-function Hello() {
+function Hello(props) {
+  const { children } = props;
   return (
     <View>
-      <Text style={styles.text}>Greatfuldays!</Text>
+      <Text style={styles.text}>
+        {`Greatfuldays! ${children}`}
+      </Text>
     </View>
   );
 }
+
+Hello.prpoypes = {
+  children: string.isRequired,
+};
 
 const styles = StyleSheet.create({
   text: {
